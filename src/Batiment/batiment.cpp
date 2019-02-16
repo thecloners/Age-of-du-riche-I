fcf297ed84ca6de38cc994dd9fecb7ac4bbf53e7
alex <toussaintalex@free.fr>
2019-02-16 12:26:49 +0100

    ajout batiments

---
+++ src/Batiment/batiment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "batiments.hpp"
+using namespace std;
+
+  void Batiments::recevoirDegats(int degats) {
+
+    pv -= degats;
+
+}
---
+++ src/Batiment/batiments.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <string>
+using namespace std;
+
+#ifndef BATIMENTS
+#define BATIMENTS
+
+class Batiments
+{
+  private:
+    int pv;
+    int taille;
+    int position;
+    
+  public:
+    void recevoirDegats (int degats);
+
+
+
+
+};
+#endif
