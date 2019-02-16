56a5801f724af55532d011bd10284ed553121e0e
m3nde <mendeleiv94@gmail.com>
2019-02-16 12:10:31 +0100

    cases

---
+++ src/partie.hpp
@@ -0,0 +1,21 @@
+#ifndef PART_
+#define PART_
+
+#include <iostream>
+
+
+class Partie
+{
+    private :
+
+ int mNbJoueur;
+ int taille_plateau;
+ int mNbtour;
+ std::string nomParti;
+
+
+    public :
+
+
+
+}
