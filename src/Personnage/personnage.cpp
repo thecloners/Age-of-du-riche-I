71ba5c7fdcb481274d16df4c100b0fbba492ef8a
alex <Ledevellopeurfous>
2019-02-16 12:08:18 +0100

    constructeur

--- src/Personnage/personnage.cpp
+++ src/Personnage/personnage.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <string>
+#include "personnage.hpp"
 using namespace std;
-Personnage::Personnage() {
+
+Personnage::Personnage()
+{
       mvie = 100;
       mVieMax = 100;
       mDegat = 100;
-      mcible = [100,100];
+      mcible = {100,100};
       mPortee = 100;
-      mPosition = [100, 100];
-}
-void Personnage::definircible(){
-
-}
-
-void Personnage::mouvement(){
-
-}
-
-void Personnage::attaque(){
-
+      mPosition = {100, 100};
 }
