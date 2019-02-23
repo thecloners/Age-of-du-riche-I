17982e380d108cc0ab4c868d199e0c2832b9c9a3
ledieumichel <alexandre.caranica@gmail.com>
2019-02-23 12:40:04 +0100

    villageois 2

---
+++ src/Personnage/villageois/villageois.cpp
@@ -0,0 +1,14 @@
+#include "villageois.hpp"
+
+
+using namespace std;
+
+Personnage::Villageois()
+{
+      mVie = 25;
+      mVieMax = 25;
+      mDegat = 3;
+      mcible = {100,100};
+      mPortee = 1;
+      mPosition = {100, 100};
+}
