082eebef5328ac9f6df13cc9d9fc732bdbbacab0
belzebalex <toussaintalex@free.fr>
2019-03-16 10:32:24 +0100

    quelques pitites corrections

--- src/Personnage/personnage.hpp
+++ src/Personnage/personnage.hpp
@@ -10,32 +10,34 @@ class Personnage //: public sf::Drawable{
 {
 
     private:
-      sf::Vector2f mPositionEnnemi;
+      sf::Vector2f mPositionEnnemi; // Ouais, pas mal. On attaque donc juste sur une position
       int mVie;
       int mVieMax;
-      sf::Vector2f mPosition;
+      sf::Vector2f mPosition; 
       int mDegat;
-      int mPortee;
-      sf::Vector2f mcible;
-      int mCoupEnOr;
+      int mPortee; // Parfait
+      sf::Vector2f mcible; // A renommer mCible
+      int mCoupEnOr; // Super les gars! Juste c'est pas coup mais cout haha
       int mCoupEnNourriture;
       int mCoupEnBois;
       int mCoupEnPierre;
       //virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
+      // Pourquoi vous avez commenté ça?
 
 
 
     public:
-      Personnage();
-      void dessiner(sf::Vector2f position);
-      void definircible();
-      void definirennemi();
-      void peutattaquercible();
-      void mouvement();
-      void attaque();
-      void getporte();
-      void evoluer();
+      Personnage(); // Il faut pouvoir passer des arguments! (les couts, la portee, etc)
+                    // Sinon la classe fille pourra pas modifier les modifier
+                    
+      void dessiner(sf::Vector2f position); // Utiliser le virtual void draw de sf::Drawable!
+      void definircible();  // A renommer definirCible(); et peut être lui passer en argument des coords?
+      void definirennemi(); // ouaais?
+      void peutattaquercible(); // peutAttaquerCible(). Je vois pas trop à quoi ça sert mais vous devez savoir
+      void mouvement();         // mmhh??
+      void attaque();           // mmhhhhhh???
+      void getporte();          // mmhmmmmhmhhh???
+      void evoluer();           // WTF?!
 
 
 
