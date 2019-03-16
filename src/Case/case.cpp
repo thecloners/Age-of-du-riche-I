c0e7328abb2004070ff8eb93b8d23a3ed11a5e12
sednasturtium <capussier@gmail.com>
2019-03-16 13:00:11 +0100

    ajout class case

--- src/Case/case.cpp
+++ src/Case/case.cpp
@@ -1,6 +1,22 @@
 #include "case.hpp"
 
-if Case (case_t mer)
-    mpasser_ou_pas_passer == 1
-
- 
+Case::Case(case_t typeCase)
+{
+    mTypeCase = typeCase;
+    if (typeCase == mer) {
+        mpasser_ou_pas_passer = 1;
+        mcouleur = sf::Color(80, 90,155);
+        }
+    if (typeCase == foret) {
+        mpasser_ou_pas_passer = 0;
+        mcouleur = sf::Color(15, 90, 15);
+        }
+    if (typeCase == plaine) {
+        mpasser_ou_pas_passer = 0;
+        mcouleur = sf::Color(120, 210, 65);
+        }
+    if (typeCase == roche) {
+        mpasser_ou_pas_passer = 1;
+        mcouleur = sf::Color(195, 170, 135);
+        }
+}
--- src/Case/case.hpp
+++ src/Case/case.hpp
@@ -1,16 +1,18 @@
 #ifndef CASE_
 #define CASE_
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 
 class Case
 {
 private:
     bool mpasser_ou_pas_passer;
-    int mcouleur;
+    sf::Color mcouleur;
 public:
     typedef enum {mer, foret, plaine, roche} case_t;
     Case(case_t typeCase);
+    case_t mTypeCase;
 };
 
 #endif
