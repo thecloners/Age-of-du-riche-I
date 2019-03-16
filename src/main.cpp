23d9f0005d68120bb6dcc5fa20fd6794690bfc72
alex <Ledevellopeurfous>
2019-03-16 12:50:00 +0100

    jw<GVBH JUK `

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include "Personnage/personnage.hpp"
-#include "Personnage/personnage.cpp"
+#include "Personnage/villageois/villageois.hpp"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "Age Of Du Pauvre v0.01");
 
-    Personnage personnage();
+    Villageois personnage();
 
     while (window.isOpen())
     {
