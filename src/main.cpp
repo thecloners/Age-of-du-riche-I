7a7a17063da631ad98a59a3cc1afcb5efeabf72e
belzebalex <toussaintalex@free.fr>
2019-03-16 09:46:30 +0100

    corrigé les erreurs et les prototypes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
-#include "personnage.hpp"
-int main(){
-    Personnage test;
-    window.draw(test);
+#include <SFML/Graphics.hpp>
+#include "partie.hpp"
+
+
+int main()
+{
+    Partie partie;
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Age Of Du Riche v0.01");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else 
+                partie.sendEvent(event);
+        }
+        partie.update();
+        window.clear();
+        window.draw(partie);
+        window.display();
+    }
+
+    return 0;
 }
