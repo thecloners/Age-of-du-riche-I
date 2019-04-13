67d23cd75cc5448db331adb7eba43d8c38dc94b6
thecloners <thibaultvictor@orange.fr>
2019-04-13 12:26:07 +0200

    hitbox

--- src/Batiment/batiments.cpp
+++ src/Batiment/batiments.cpp
@@ -16,22 +16,32 @@ Batiment::Batiment (bat_t TypeBat, sf::Vector2f Position)
     switch(mTypeBat)
     {
         case maison:
+            mTailleX = 30;
+            mTailleY = 50;
             mPV = 500;
             mColor = sf::Color::Green;
+
             break;
         case moulin:
+            mTailleX = 50;
+            mTailleY = 100;
             mPV = 250;
             mColor = sf::Color::Blue;
             break;
         case chateau:
+            mTailleX = 100;
+            mTailleY = 150;
             mPV = 1500;
             mColor = sf::Color::Red;;
             break;
         default:
+            mTailleX = 0;
+            mTailleY = 0;
             mPV = 1;
             mColor = sf::Color::Black;
         break;
     }
+mHitboxDefensive(mPosition,mTailleX,mTailleY);
 }
 
 Batiment::~Batiment(){
@@ -57,10 +67,7 @@ sf::Time timeBat = sf::milliseconds(1000);
     //if mPv
     }
 
-// Alors cette fonction la permet de faire un window.draw(batiment)
-// RenderTarget représente la window
-// Donc si vous avez un RectangleShape à dessiner,
-// Vous faites RenderTarget.draw(tiboInShape);
+
 void Batiment::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
     sf::CircleShape mTriangle(80.f, 3);
--- src/Batiment/batiments.hpp
+++ src/Batiment/batiments.hpp
@@ -8,25 +8,24 @@
 
 typedef enum {maison, moulin, chateau} bat_t;
 
-// La classe représente UN batiment, doncn pas Batiments mais Batiment!!!
-class Batiment : public sf::Drawable // La on hérite de Drawable pour pouvoir se faire dessienr. Valide!
+
+class Batiment : public sf::Drawable
 
 {
   private:
     int mPV;
-    int mTAILLE;
+    unsigned int mTailleX,mTailleY;
     sf::Vector2f mPosition;
     sf::Color mColor;
     bat_t mTypeBat;
+    hitbox mHitboxDefensive();
 
   public:
 
     Batiment(bat_t nom, sf::Vector2f Position);
-    Batiment::~Batiment()
-    // Alors cette fonction la permet de faire un window.draw(batiment)
-    // RenderTarget représente la window
-    // Donc si vous avez un RectangleShape à dessiner,
-    // Vous faites RenderTarget.draw(tiboInShape);
+    Batiment::~Batiment();
+
+
     virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 
     void recevoirDegats(int degats);
