ad302bc8edcabea85eed59456cc21a4da42675c6
sednasturtium <capussier@gmail.com>
2019-04-13 11:29:27 +0200

    add blank constructor hitbox

--- src/hitbox.cpp
+++ src/hitbox.cpp
@@ -2,20 +2,32 @@
 
 Hitbox::Hitbox(sf::Vector2f p1,sf::Vector2f p2,sf::Vector2f p3,sf::Vector2f p4)
 {
-    mArrete1 = p1;
-    mArrete2 = p2;
-    mArrete3 = p3;
-    mArrete4 = p4;
+    mSommet1 = p1;
+    mSommet2 = p2;
+    mSommet3 = p3;
+    mSommet4 = p4;
+
+
 }
 
 Hitbox::Hitbox(sf::Vector2f p, sf::Vector2f taille)
 {
-    mArrete1 = p;
-    mArrete2.x = p.x + taille.x;
-    mArrete2.y = p.y;
-    mArrete3 = p + taille;
-    mArrete4.x = p.x;
-    mArrete4.y = p.y + taille.y;
+    mSommet1 = p;
+    mSommet2.x = p.x + taille.x;
+    mSommet2.y = p.y;
+    mSommet3 = p + taille;
+    mSommet4.x = p.x;
+    mSommet4.y = p.y + taille.y;
+}
+
+Hitbox::Hitbox()
+{
+    sf::Vector2f p(0.f,0.f);
+    mSommet1 = p;
+    mSommet2 = p;
+    mSommet3 = p;
+    mSommet4 = p;
+
 }
 
 /*int Hitbox::collision()
--- src/hitbox.hpp
+++ src/hitbox.hpp
@@ -10,10 +10,11 @@ class Hitbox : public sf::Drawable
 {
   private:
 
-      sf::Vector2f mArrete1;
-      sf::Vector2f mArrete2;
-      sf::Vector2f mArrete3;
-      sf::Vector2f mArrete4;
+      sf::Vector2f mSommet1;
+      sf::Vector2f mSommet2;
+      sf::Vector2f mSommet3;
+      sf::Vector2f mSommet4;
+
   public:
     bool mAction;
 
@@ -22,5 +23,29 @@ class Hitbox : public sf::Drawable
 };
 //lien collisions
 //https://openclassrooms.com/fr/courses/1374826-theorie-des-collisions/1375352-formes-plus-complexes
+
+Vector2f Tsommet [4]={mSommet1,mSommet2,mSommet3,mSommet4};
+
+
+  int i;
+  for(i=0;i<4;i++)
+  {
+     Point A = tab[i];
+     Point B;
+     if (i==4-1)  // si c'est le dernier point, on relie au premier
+         B = tab[0];
+     else           // sinon on relie au suivant.
+         B = tab[i+1];
+     Vecteur D,T;
+     D.x = B.x - A.x;
+     D.y = B.y - A.y;
+     T.x = P.x - A.x;
+     T.y = P.y - A.y;
+     float d = D.x*T.y - D.y*T.x;
+     if (d<=0)
+        return false;  // un point à droite et on arrête tout.
+  }
+  return true;  // si on sort du for, c'est qu'aucun point n'est à gauche, donc c'est bon.
+}
 
 #endif
